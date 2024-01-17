b15aef8079ac159b19706a9b78952ae233d7c856
Marco Milza <marco@MacBook-Pro-di-Marco.local>
2024-01-17 23:42:15 +0100

    feat: first composing done

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,15 @@
-console.log("hello world Marco Milza");
+type IPlusOne = (n: number) => number;
+type IToStr = (n: number) => string;
+
+const plus1: IPlusOne = n => n + 1;
+const toStr: IToStr = n => n.toString();
+
+type ICompose = (n: number) => string;
+
+const plus1_then_toStr: ICompose = n => toStr(plus1(n));
+
+console.log(
+  `marcom ---> plus1_then_toStr(3): `,
+  plus1_then_toStr(3),
+  typeof plus1_then_toStr(3),
+);
